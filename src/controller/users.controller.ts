da2386cb0ea5c55ae5922f67136b0b8ee23beabf
Sanjay Vuppugandla <sanjay.uppugandla@gmail.com>
2025-05-14 23:21:42 -0700

    Added the post method for the same users route

--- src/controller/users.controller.ts
+++ src/controller/users.controller.ts
@@ -14,3 +14,15 @@ export const logger2 = (req: Request, res : Response, next : NextFunction) => {
     console.log(responseObject);
     res.send(responseObject);
 }
+
+export const logger3 = (req : Request, res: Response, next : NextFunction) => {
+    const nameParam = req.params.name;
+    console.log(`The name of the user we are adding is ${nameParam}`);
+    next();
+}
+
+export const logger4 = (req : Request, res : Response, next : NextFunction) => {
+    console.log("Logging the last method");
+    res.send("The response to the logger 4");
+}
+
